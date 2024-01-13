3e7ed2e2a612ada23ddefd98082b74c27f9f97ba
Mark Landeryou <samurai96@hotmail.com>
2024-01-13 05:22:06 -0500

    refactor: added a copyright component

---
+++ src/components/Footer/Copyright.tsx
@@ -0,0 +1,10 @@
+const Copyright = () => {
+  return <div>Copyright</div>;
+};
+
+export default Copyright;
+// function date() {
+//   const currentDate = new Date();
+//   const year = currentDate.getFullYear();
+//   console.log(year);
+// }
