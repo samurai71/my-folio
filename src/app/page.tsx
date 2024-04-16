87828db8545f4787e6c78c6fe749986afb4f4e8b
Mark Landeryou <samurai96@hotmail.com>
2024-04-16 00:51:30 -0400

    refactor: changed some of the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="font-bold font-serif text-4xl text-center">
+      <h1 className="text-center font-serif text-4xl font-bold">
         Mark Landeryou&apos;s Personal Portfolio
       </h1>
     </main>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="border-2 mt-2">
+    <footer className="mt-2 border-2">
       <p className="text-center">
         &copy; {new Date().getFullYear()} All rights reserved.
       </p>
