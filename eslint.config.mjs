20520a1ebb40bf751e627bb053d08fbf377736b5
Mark Landeryou <samurai96@hotmail.com>
2024-05-10 17:38:34 -0400

    chore: made some edits to the eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,20 @@
-// eslint.config.js
-export default [
-  {
-    rules: {
-      "prefer-const": "error",
-    },
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:tailwindcss/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-];
+};
